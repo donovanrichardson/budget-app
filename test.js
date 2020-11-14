8e15c799227c8f0fd8edd2652483a70aee0f89be
donovanrichardson <donovanspqa@gmail.com>
2020-11-14 11:12:33 -0500

    experimenting with luxon

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 require('dotenv').config()
 const mongoose = require('mongoose')
-const Statement = require('./models/statement')
+const {Statement} = require('./models/statement')
+const {DateTime} = require('luxon')
 
 mongoose.connect('mongodb://localhost/my_database', {
   useNewUrlParser: true,
@@ -10,18 +11,28 @@ mongoose.connect('mongodb://localhost/my_database', {
   useCreateIndex: true
 });
 
+async function budget(){
+    const budg = await Statement.create({
+        owe: 0,
+        expend: 0,
+        date: DateTime.local()
+    })
+    console.log(budg);
+    await budg.deleteOne()
+}
+
 async function inq(){
     const result = await inquirer.prompt([
         {
             name:"option",
             type: "list",
-            message:"What do you want?",
-            choices:['dotenv', 'find']
+            message:"What do you owe?",
+            choices:['budget', 'find']
         }
     ])
 
-    if (result.option === 'dotenv'){
-        console.log(process.env.DATABASE_URL);
+    if (result.option === 'budget'){
+        await budget()
     } else if (result.dotenv === 'find'){
         const found = await Statement.find()
     }
